fd60024872203e42842e3bcbe72241e6e150bdab
Abhilaksh <imabhilaksh@gmail.com>
2021-07-26 15:01:25 +0530

    Changed precip to cloud cover

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,33 @@
-const request = require('request')
-const chalk=require('chalk')
+const request = require("request");
+const chalk = require("chalk");
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=f7e3d6130ddfea490a212ea8c1252763&query=' + latitude + ',' + longitude + '&units=m'
-    request({ url, json: true }, (error, {body}) => {
-        if (error) {
-            callback('Unable to connect to weather service.',undefined)
-        }
-        else if (body.error) {
-            callback('Unable to find location.',undefined)
-        }
-        else {
-            callback(undefined,body.current.weather_descriptions[0]+"."+" It is currently "+body.current.temperature+" degrees out there."+" There is a "+body.current.precip+" chance of rain.")
-//             callback(undefined,`${body.current.weather_descriptions[0]}
-// It is currently ${body.current["temperature"]} degrees out there. There is a ${body.current["precip"]} percent chance of rain.`)
-            
-        }
-    })
-}
+	const url =
+		"http://api.weatherstack.com/current?access_key=f7e3d6130ddfea490a212ea8c1252763&query=" +
+		latitude +
+		"," +
+		longitude +
+		"&units=m";
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback("Unable to connect to weather service.", undefined);
+		} else if (body.error) {
+			callback("Unable to find location.", undefined);
+		} else {
+			callback(
+				undefined,
+				body.current.weather_descriptions[0] +
+					"." +
+					" It is currently " +
+					body.current.temperature +
+					" degrees out there." +
+					" There is a cloud cover of : " +
+					body.current.cloudcover +
+					" today."
+			);
+			//             callback(undefined,`${body.current.weather_descriptions[0]}
+			// It is currently ${body.current["temperature"]} degrees out there. There is a ${body.current["precip"]} percent chance of rain.`)
+		}
+	});
+};
 
-module.exports=forecast
+module.exports = forecast;
